f2376315f66666110d9010921a3b67368ced68f8
Yimmi-urbano <139839538+Yimmi-urbano@users.noreply.github.com>
2024-05-13 20:16:32 -0500

    fix/path images url

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ const slugify = require('slugify');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const BRIAMZSPORT_DIR = '../media-static.creceidea.pe';
+const DIRECTORY_BASE = '../media-static.creceidea.pe';
 
 const ensureDirectoriesExist = async (DIR_DOMAIN) => {
     try {
-        await fs.access(`${BRIAMZSPORT_DIR}/${DIR_DOMAIN}`);
+        await fs.access(`${DIRECTORY_BASE}/${DIR_DOMAIN}`);
     } catch (err) {
         if (err.code === 'ENOENT') {
-            await fs.mkdir(`${BRIAMZSPORT_DIR}/${DIR_DOMAIN}`, { recursive: true });
+            await fs.mkdir(`${DIRECTORY_BASE}/${DIR_DOMAIN}`, { recursive: true });
         } else {
             console.error('Error al acceder al directorio:', err);
             throw err; // Lanzar el error para que sea manejado en la ruta /upload
@@ -35,8 +35,8 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage: storage });
 
-app.post('/upload', upload.single('image'), async (req, res) => {
-    const DIR_DOMAIN = 'fiberstar';
+app.post('/upload/banner', upload.single('image'), async (req, res) => {
+    const DIR_DOMAIN = 'fiberstar/images/banner';
     try {
         await ensureDirectoriesExist(DIR_DOMAIN); // Esperar a que se aseguren los directorios existan
 
@@ -50,7 +50,7 @@ app.post('/upload', upload.single('image'), async (req, res) => {
             return res.status(400).send('Formato de imagen no permitido.');
         }
 
-        const outputPath = `${BRIAMZSPORT_DIR}/${DIR_DOMAIN}/${slugify(req.file.originalname.split('.')[0], { lower: true, remove: /[*+~.()'"!:@]/g }).replace(/\s+/g, '-')}.webp`;
+        const outputPath = `${DIRECTORY_BASE}/${DIR_DOMAIN}/${slugify(req.file.originalname.split('.')[0], { lower: true, remove: /[*+~.()'"!:@]/g }).replace(/\s+/g, '-')}.webp`;
         await sharp(req.file.path).toFormat('webp').toFile(outputPath);
         await fs.unlink(req.file.path); // Eliminar el archivo original con fs.promises
 
@@ -61,7 +61,33 @@ app.post('/upload', upload.single('image'), async (req, res) => {
     }
 });
 
-app.use(express.static(BRIAMZSPORT_DIR));
+app.post('/upload/products', upload.single('image'), async (req, res) => {
+    const DIR_DOMAIN = 'fiberstar/images/products';
+    try {
+        await ensureDirectoriesExist(DIR_DOMAIN); // Esperar a que se aseguren los directorios existan
+
+        if (!req.file) {
+            return res.status(400).send('No se ha subido ninguna imagen.');
+        }
+
+        const allowedFormats = ['image/jpeg', 'image/png'];
+        if (!allowedFormats.includes(req.file.mimetype)) {
+            await fs.unlink(req.file.path); // Usar fs.promises para eliminar el archivo
+            return res.status(400).send('Formato de imagen no permitido.');
+        }
+
+        const outputPath = `${DIRECTORY_BASE}/${DIR_DOMAIN}/${slugify(req.file.originalname.split('.')[0], { lower: true, remove: /[*+~.()'"!:@]/g }).replace(/\s+/g, '-')}.webp`;
+        await sharp(req.file.path).toFormat('webp').toFile(outputPath);
+        await fs.unlink(req.file.path); // Eliminar el archivo original con fs.promises
+
+        res.send({ imageUrl: `http://localhost:${PORT}/${outputPath}` });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error interno del servidor.');
+    }
+});
+
+app.use(express.static(DIRECTORY_BASE));
 
 app.listen(PORT, () => {
     console.log(`Servidor en funcionamiento en http://localhost:${PORT}`);
